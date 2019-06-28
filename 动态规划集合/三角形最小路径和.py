f775073337ab5fa68943521aa0f3cedb5349b761
xyb1996全局系统用户名 <1464218823@qq.com>
2019-06-28 15:52:10 +0800

    dp

---
+++ 动态规划集合/三角形最小路径和.py
@@ -0,0 +1,20 @@
+"""
+给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+例如，给定三角形：
+
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+"""
+
+class Solution(object):
+	def minimumTotal(self, triangle):
+		length = len(triangle)
+		dp=[]
+		dp.append(triangle[0][0])
+		for i in range(1,length):
+			dp.append()
